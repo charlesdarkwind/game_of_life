9524d5b2a647478f461175c9744f1a4364a13e3f
charlesdarkwind <guildwarsguy12@gmail.com>
2017-07-02 09:44:53 -0400

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -2,44 +2,42 @@ import React, { Component } from 'react';
 import '../css/index.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    //this.generateRow = this.generateRow.bind(this);
+  constructor(props) {
+    super(props)
+    this.generateGrid = this.generateGrid.bind(this); 
+    this.state = {
+      grid: [],
+      next: []
+    } 
   }
 
-  generateRow() {
-    
-  }
-
-  componentDidMount() {
-    const grid = document.querySelector('.grid');
-    console.log(grid);
-    for (let i = 0; i < 67; i++) {
-      grid.appendChild(document.createElement('div'));
+  generateGrid() {
+    for (let i = 0; i < 3500; i++) {
+      this.state.grid.push(<div className="col" key={i}></div>)
     }
+    return this.state.grid;
   }
-/*
-  generateRowCell(id) {
-    console.log('hi');
-    return '<div className="col"></div>';
-  }*/
+
+  start() {
+    console.log('hi')
+  }
 
   render() {  
-    return (
+    return (     
       <div className="App">
         <div className="App-header">
         </div>
         <div className="App-body">
           <div className="grid-wrap">
             <div className="grid">
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
-              {/*this.generateRow()*/}
+              {
+                this.generateGrid()
+              }
             </div>
           </div>
         </div>
         <div className="App-footer">
+          <button className="start" onClick={() => this.start()}>Start</button>
         </div>
       </div>
     );
