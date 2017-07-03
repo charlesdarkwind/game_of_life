0a06d65d7734dfe7e7676bd4191db11d40e35161
charlesdarkwind <guildwarsguy12@gmail.com>
2017-07-03 10:31:53 -0400

    oi

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,23 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.generateGrid = this.generateGrid.bind(this); 
+    this.updateGrid = this.updateGrid.bind(this);
     this.start = this.start.bind(this);
+    this.newGeneration = this.newGeneration.bind(this);
 
     // The grid is a 2 dimensional array of rows and columns
     this.state = {
       initialized: false,
       grid: [],
-      next: [],
+      nextGrid: [],
+      livingCells: [],
       height: 0,
       width: 0
-    } 
+    }; 
   }
 
   componentWillMount() {
-    return this.state.initialized ? null : this.generateGrid(50, 70);
+    this.updateGrid();
   }
 
   generateGrid(height, width) {
@@ -35,7 +38,6 @@ class App extends Component {
           />);
       }    
     }
-
     this.setState({
       initialized: true,
       grid,
@@ -44,15 +46,27 @@ class App extends Component {
     });
   }
 
+  updateGrid() {
+    return this.state.initialized ? this.state.grid : this.generateGrid(50, 70);
+  }
+
   start() {
-    console.log(this.determineNeighbours([50, 70]));
+    const copy = this.state.grid;
+    copy[30][30] = <Cell          
+            key={[30, 30]}
+            id={[30, 30]}
+            status={'alive'}
+          />;
+    this.setState({
+      grid: copy
+    });
   }
 
   determineNeighbours(cell) {
 
     // Determines the surrounding neighbours of a cell
     // and "wraps" the edges of the grid resulting 
-    // in an infinite grid effect.
+    // in an infinite grid effect
 
     //  0 1 2
     //  3   4
@@ -67,7 +81,7 @@ class App extends Component {
     const neighbourLeftCol = () => col - 1 === -1 ? width : col - 1;
     const neighbourRightCol = () => col + 1 === width + 1 ? 0 : col + 1;
 
-    const arr = [
+    return [
       [neighbourTopRow(), neighbourLeftCol()], 
       [neighbourTopRow(), col], 
       [neighbourTopRow(), neighbourRightCol()], 
@@ -77,44 +91,14 @@ class App extends Component {
       [neighbourBotRow(), col], 
       [neighbourBotRow(), neighbourRightCol()]
     ];
+  }
 
-    /*
-    switch(i) {
-      case 0:
-      // row - 1, col - 1 
-        arr.push([neighbourTopRow(), neighbourLeftCol()]);
-        break;
-      case 1:
-      // row - 1, same col
-        arr.push([neighbourTopRow(), col]);
-        break;
-      case 2:
-      // row - 1, col + 1
-        arr.push([neighbourTopRow(), neighbourRightCol()]);
-        break;
-      case 3:
-      // same row, col - 1
-        arr.push([row, neighbourLeftCol()]);
-        break;
-      case 4:
-        // same row, col + 1
-        arr.push([row, neighbourRightCol()]);
-        break;
-      case 5:
-        // row + 1, col - 1
-        arr.push([neighbourBotRow(), neighbourLeftCol()]);
-        break;
-      case 6:
-        // row + 1, same col
-        arr.push([neighbourBotRow(), col]);
-        break;
-      case 7:
-        // row + 1, col + 1
-        arr.push([neighbourBotRow(), neighbourRightCol()]);
-        break;
-    }
-    */
-    return arr;
+  newGeneration() {
+
+  }
+
+  addLivingCell() {
+
   }
 
   render() {  
@@ -125,12 +109,13 @@ class App extends Component {
         <div className="App-body">
           <div className="grid-wrap">
             <div className="grid">
-              {this.state.grid}
+              {this.updateGrid()}
             </div>
           </div>
         </div>
         <div className="App-footer">
           <button className="start" onClick={() => this.start()}>Start</button>
+          <button className="newGenButton" onClick={() => this.newGeneration()}>New generation</button>
         </div>
       </div>
     );
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,13 +4,23 @@ import '../css/index.css';
 class Cell extends Component {  
   constructor(props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       status: this.props.status // dead, alive, trans-live, trans-dead 
     }
   }
+
+  handleClick() {
+    const next = this.state.status === 'dead' ? 'alive' : 'dead';
+    //this.props.addLivingCell(this.props.id);
+    this.setState({
+      status: next
+    });
+  }
+
   render() {  
     return (     
-      <div className={`Cell ${this.props.status}` } onClick={() => console.log(this.props.id)}></div>
+      <div className={`Cell ${this.state.status}` } onClick={() => this.handleClick()}></div>
     );
   }
 }
